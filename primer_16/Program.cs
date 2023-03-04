0785286c1c8d72ea90317e6782f1b0fbb04cabf7
Nikita Isakov <neta782278@yandex.ru>
2023-03-04 10:23:59 +0700

    16

---
+++ primer_16/Program.cs
@@ -0,0 +1,13 @@
+﻿// int factorial(int n)
+// {
+//     if(n==1) return 1;
+//     else  return n*factorial(n-1);
+// }
+// Console.WriteLine(factorial(5));
+
+int fib(int n)
+{
+    if(n==1 || n==2) return 1;
+    else return fib(n-1)+fib(n-2);
+}
+Console.WriteLine(fib(10));
